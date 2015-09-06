1ebc37500ac2c99b8759eaadb025b6a7e0e08cc2
dawidkmiecik <dawidkmiecik@vp.pl>
2015-09-06 21:09:39 +0200

    Commit Project PN

---
+++ include/nav.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Procjet Piotr Nowak</title>
+		<link href="css/style.css" rel="stylesheet">
+
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script> 
+		<script src="js/transition.js"></script>
+		<script src="js/affix.js"></script>
+		<script src="js/alert.js"></script>
+		<script src="js/button.js"></script>
+		<script src="js/carousel.js"></script>
+		<script src="js/collapse.js"></script>
+		<script src="js/modal.js"></script>
+		<script src="js/popover.js"></script>
+		<script src="js/dropdown.js"></script>
+		<script src="js/tab.js"></script>
+		<script src="js/tooltip.js"></script>
+		<script src="js/transition.js"></script>
+	</head>
+	<body>
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		  <!-- Grupowanie "marki" i przycisku rozwijania mobilnego menu -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Rozwiń nawigację</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">"Marka - Logo strony"</a>
+		    </div>
+		    <!-- Brand and toggle get grouped for better mobile display -->
+			<!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Start</a></li>
+		        <li><a href="#">O nas</a></li>
+		       	<li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Masaż <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		          </ul>
+		        </li> 
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Rehabilitacja <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		          </ul>
+		        </li> 
+		        <li><a href="#">Kontakt</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+
