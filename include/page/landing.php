0c26a577558746117c10f6c88a8494249c28c82a
dawidkmiecik <dawidkmiecik@vp.pl>
2015-09-08 00:31:54 +0200

    Create massage_in.php

---
+++ include/page/landing.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Procjet Piotr Nowak</title>
+		<link href="css/style.css" rel="stylesheet">
+
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script> 
+		<script src="js/transition.js"></script>
+		<script src="js/affix.js"></script>
+		<script src="js/alert.js"></script>
+		<script src="js/button.js"></script>
+		<script src="js/carousel.js"></script>
+		<script src="js/collapse.js"></script>
+		<script src="js/modal.js"></script>
+		<script src="js/popover.js"></script>
+		<script src="js/dropdown.js"></script>
+		<script src="js/tab.js"></script>
+		<script src="js/tooltip.js"></script>
+		<script src="js/transition.js"></script>
+	</head>
+	<body>
+		<div class="container"> 
+			<div class="row">
+				<div class="col-md-4 col-md-offset-2">
+					<img src="img/logo.png" alt="logo">
+				</div>
+				<div class="col-md-4 col-md-offset-2">
+					<h2>ZADZWOŃ: 888 888 888</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<form class="col-md-4 col-md-offset-2" role="form">
+						<div class="form-group">
+							<label for="name">Imię </label>
+							<input type="password" class="form-control" id="name" placeholder="Wpisz swoje imię">
+						</div>
+						<div class="form-group">
+							<label for="email">Email</label>
+							<input type="email" class="form-control" id="email" placeholder="Podaj adres e-mail">
+						</div>
+						<div class="form-group">
+							<label for="phoneNumber">Numer telefonu</label>
+							<input type="tel" class="form-control" id="phoneNumber" placeholder="Poadj numer telefonu">
+						</div>
+							<button type="submit" class="btn btn-default">Wyślij!</button>
+					</form>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">.
+					<img src="img/headerFoto1.png" alt="logo">
+					<h2 class="text-center">Header 1</h2>
+					<p class="text-justify">iet quisquam voluptate rem debitis harum, iure, id culpa ullam autem enim dolores recusandae, magnam tempora molestias nesciunt commodi suscipit expedita consequatur maiores vero veritatis, ea voluptates. Nisi error ut id distinctio officiis temporibus rem fugit aspernatur eligendi sequi sed pariatur illum, doloribus tempora, dolor sit vero quis omnis cumque aliquam velit tenetur possimus similique! Quam blanditiis sint perferendis reiciendis quisquam, velit nobis rem, sunt, laborum corporis eum ipsum ex at quo ratione quas nam maiores laudantium adipisci nesciunt reprehenderit veritatis ea asperiores odit. Facilis, necessitatibus, optio.</p>
+				</div>
+				<div class="col-md-4">.
+					<img src="img/headerFoto2.png" alt="logo">
+					<h2 class="text-center">Header 2</h2>
+					<p class="text-justify">iet quisquam voluptate rem debitis harum, iure, id culpa ullam autem enim dolores recusandae, magnam tempora molestias nesciunt commodi suscipit expedita consequatur maiores vero veritatis, ea voluptates. Nisi error ut id distinctio officiis temporibus rem fugit aspernatur eligendi sequi sed pariatur illum, doloribus tempora, dolor sit vero quis omnis cumque aliquam velit tenetur possimus similique! Quam blanditiis sint perferendis reiciendis quisquam, velit nobis rem, sunt, laborum corporis eum ipsum ex at quo ratione quas nam maiores laudantium adipisci nesciunt reprehenderit veritatis ea asperiores odit. Facilis, necessitatibus, optio.</p>
+				</div>
+				<div class="col-md-4">.
+					<img src="img/headerFoto3.png" alt="logo">
+					<h2 class="text-center">Header 3</h2>
+					<p class="text-justify">iet quisquam voluptate rem debitis harum, iure, id culpa ullam autem enim dolores recusandae, magnam tempora molestias nesciunt commodi suscipit expedita consequatur maiores vero veritatis, ea voluptates. Nisi error ut id distinctio officiis temporibus rem fugit aspernatur eligendi sequi sed pariatur illum, doloribus tempora, dolor sit vero quis omnis cumque aliquam velit tenetur possimus similique! Quam blanditiis sint perferendis reiciendis quisquam, velit nobis rem, sunt, laborum corporis eum ipsum ex at quo ratione quas nam maiores laudantium adipisci nesciunt reprehenderit veritatis ea asperiores odit. Facilis, necessitatibus, optio.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="text-center">Poznaj nas</h2>					
+					<div class="embed-responsive embed-responsive-16by9">
+					  	<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/c4BLVznuWnU"></iframe>	
+					</div>
+				</div>
+			</div>
+		</div>
+</html>
+
+
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php include 'include/page/landing.php'; ?>
 <?php include 'include/nav.php'; ?> 
 <?php include 'include/main/start.php'; ?> 
 <?php include 'include/footer.php'; ?> 
+
+
