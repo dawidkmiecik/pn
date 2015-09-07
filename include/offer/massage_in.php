f353cb1ede4ffefa7e041932d936f7a2d2e7a4c2
dawidkmiecik <dawidkmiecik@vp.pl>
2015-09-08 00:31:54 +0200

    Create massage_in.php

--- include/offer/massage_in.php
+++ include/offer/massage_in.php
@@ -1,6 +1,45 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
+			<h2 class="text-center">Masaże</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum dignissimos deleniti iure inventore aspernatur voluptas explicabo eligendi dolorem excepturi commodi neque totam atque itaque recusandae adipisci, nesciunt est soluta sed suscipit minus quis molestias ea illo. Magni totam, doloribus autem? .Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, laborum deleniti minus natus molestiae eaque expedita quaerat blanditiis adipisci molestias nisi excepturi laudantium placeat maxime voluptate officiis in! Qui, facilis?</p>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<ul class="nav nav-tabs nav-justified" role="tablist">
+  				<li class="active"><a href="#Masaz1" role="tab" data-toggle="tab">Masaż klasyczny</a></li>
+  				<li><a href="#Masaz2" role="tab" data-toggle="tab">Masaż XXXX</a></li>
+  				<li><a href="#Masaz3" role="tab" data-toggle="tab">Masaż YYYY</a></li>
+  				<li><a href="#Masaz4" role="tab" data-toggle="tab">Masaż ZZZZZ</a></li>
+  				<li><a href="#Masaz5" role="tab" data-toggle="tab">Masaż GGGGG</a></li>
+			</ul>
+
+			<div class="tab-content">
+			  <div class="tab-pane active" id="Masaz1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo ullam voluptatum, iure illo pariatur dolores, laboriosam beatae qui et odio expedita nemo quo in quod dignissimos corporis ea tenetur necessitatibus.
+			  <img class="img-responsive" src="http://dummyimage.com/600x400/d12ed1/323dd1" alt="">
+			  </div>
+			  <div class="tab-pane" id="Masaz2">Zawartość drugiej karty Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae eius, quis autem in aperiam quia amet veniam. Consectetur, quis tempora, repellat dolore aspernatur dolorem quibusdam possimus incidunt ab deleniti aliquid.
+			  <img class="img-responsive" src="http://dummyimage.com/600x400/d12ed1/323dd1" alt="">
+			  </div>
+			  <div class="tab-pane" id="Masaz3">Zawartość trzeciej karty Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque accusamus quaerat nemo voluptas, saepe ipsum reiciendis voluptatum eius, sapiente porro sunt pariatur provident repudiandae! Delectus mollitia aperiam, ipsa ipsum cum!
+			  </div>
+			  <div class="tab-pane" id="Masaz4">Zawartość czwartej karty Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eligendi quo reprehenderit. Quis, ipsam nulla. Culpa ab rerum sapiente explicabo cumque? Itaque quidem temporibus exercitationem, vitae quia voluptate. Sit, at.</div>
+			  <div class="tab-pane" id="Masaz5">Zawartość czwartej karty Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eligendi quo reprehenderit. Quis, ipsam nulla. Culpa ab rerum sapiente explicabo cumque? Itaque quidem temporibus exercitationem, vitae quia voluptate. Sit, at.</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<br /> 
+<br />
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
 			<div class="panel-group" id="accordion">
 			  <div class="panel panel-default">
 			    <div class="panel-heading">
