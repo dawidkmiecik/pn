31c8de0f059853ceb00efe5c3448f04095c63f5d
dawidkmiecik <dawidkmiecik@vp.pl>
2015-09-10 22:58:04 +0200

    Correct start.php, now is good and too footer.php and styles

--- include/footer.php
+++ include/footer.php
@@ -1,2 +1,61 @@
+<div class="container">
+	<div class="row">
+		<footer>
+			<div class="col-sm-6">
+				<div class="formularz" class="padding-bottom">
+					<form method="post" action="include/main/enrollment.php">
+						<div class="col-sm-6">
+							<label>Imię i nazwisko</label>
+							<input name="name" placeholder="Twoje imie i nazwisko">
+						</div>
+
+						<div class="col-sm-6">
+							<label>Telefon</label>
+							<input name="phone" type="phone" placeholder="Twój telefon">
+						</div>
+
+						<div class="col-xs-12">
+							<label>Wiadomość</label>
+							<textarea name="message" placeholder="Napisz tu wiadomość, jeśli brakuje Ci miejsca pociągij za dolny prawy róg."></textarea>
+						</div>
+
+						<div class="col-xs-12">
+							<input id="submit" name="submit" type="submit" value="Wyślij">
+						</div>
+					</form>
+				</div>
+			</div>
+
+
+
+
+			<div class="col-md-6">
+				<address>
+					<h4 class="text-right">Piotr Nowak Rehabilitacja i Masaże</h3>
+					<h5 class="text-right">Dojazd cały Poznań button z mapą albo coś takiego żeby się ladowała i byl pokazany cały dojazd albo mapa pomiedzy tym i formularzem, ale moze nie wejsc.
+					<br />
+					<br />
+					<abbr title="Phone"></abbr><span class="glyphicon glyphicon-phone" aria-hidden="true"></span>  +48 888 888 888<br>
+					<a href="index.php"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span>  www.example.pl</a><br>
+					<a href="mailto:#"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>  biuro@exampel.pl</a>
+					</h5>
+
+				</address>	
+			</div>
+		</footer>
+	</div>
+</div>
+
+
+<div class="container">
+	<div class="row">
+		<div class="clearfix">
+			<h6 class="pull-left">Created by</h6>
+			<h6 class="pull-right"><a target="_blank" href="https://www.feniksdesign.pl">Feniks Design</a> <a href="https://www.facebook.com/FeniksDesignPoznan?fref=ts" target="_blank" ><span class="websymbol">f</span></a></h6>
+		</div>
+
+	</div>
+</div>
+
 	</body>
 </html>
